c3a89a06c0b0874108a9403f1d58136f6e34fa47
hanoon-07 <muhhamedhanoon@gmail.com>
2025-08-14 20:14:50 +0530

    check

--- rsa.py
+++ rsa.py
@@ -52,6 +52,52 @@ def compute_private_exponent(a, p, q):
     return b
     
         
+def modular_exponent(x, a, n):
+    if a == 0:
+        return 1
+    elif a % 2 == 1:
+        return x * modular_exponent(x, a - 1, n) % n
+    else:
+        half = modular_exponent(x, a // 2, n)
+        return half * half % n
 
+
+def encrypt(x, a, n):
+    return modular_exponent(x, a, n)
+
+
+def decrypt(y, b, n):
+    return modular_exponent(y, b, n)
         
+
+p, q = generate_prime_factors(factor_bits=128, min_factor_delta_bits=32)
+n = p * q
+a = generate_public_exponent(p, q)
+b = compute_private_exponent(a, p, q)
+
+print("p = ", p)
+print("q = ", q)
+print("n = ", n)
+print("a = ", a)
+print("b = ", b)
+print()
+print(f"Public Key:")
+print(f"  n={n}")
+print(f"  a={a}")
+print()
+print(f"Private Key:")
+print(f"  p={p}")
+print(f"  q={q}")
+print(f"  b={b}")
+print()
+
+x = 1234567890
+y = encrypt(x, a, n)
+decrypted = decrypt(y, b, n)
+
+print(f"Let's encrypt a number: {x}")
+print(f"Encrypted: {y}")
+print(f"Decrypted: {decrypted}")
+
+assert(x == decrypted)
 
